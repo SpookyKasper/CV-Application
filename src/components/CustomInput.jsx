61fc62352ab3e2f14fa66970270ccf9248fd8145
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-08-09 11:55:09 +0200

    Create basic custom input

---
+++ src/components/CustomInput.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+const CustomInput = ({ fieldName, initialValue = "", type = "text" }) => {
+  const [value, setValue] = useState(initialValue);
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <div className="input-field">
+      <label for={fieldName}>{fieldName}</label>
+      <input type={type} value={value} onChange={handleChange} id={fieldName} />
+    </div>
+  );
+};
+
+export default CustomInput;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
+import CustomInput from "./components/input";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <CustomInput initialValue={"peak"} />
   </StrictMode>
 );
